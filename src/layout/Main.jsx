c903961138fb1006b500ab97323d185b281ba9bb
Naimur Reza <naimur.rezaa@gmail.com>
2023-10-07 00:27:23 +0600

    added home images with logo

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -8,19 +8,19 @@ import "./Main.css";
 const Main = () => {
   return (
     <div className="flex flex-col min-h-screen px-14 py-12 transition-all">
-      <video
+      {/* <video
         src={videoBackground}
         autoPlay
         loop
         muted
         className="absolute inset-0 w-full h-full object-cover z-0"
-      />
+      /> */}
       <div className="flex-grow flex justify-between items-start ">
         <div className="z-10">
           <SideNav />
         </div>
         <img src={logo} className="w-20 z-10" alt="" />
-        <div className="flex-grow flex  justify-center items-center  absolute inset-0  fade-bg">
+        <div className="flex-grow flex  justify-center items-center  absolute inset-0  bg-black">
           <Outlet />
         </div>
       </div>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import logo from "../assets/logo.svg";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-[420px] h-[420px] relative">
+      <img
+        className="w-full h-full "
+        src="https://dfvc.com/wp-content/themes/dfvc/dist/images/large_circle.svg"
+        alt=""
+      />
+      <img
+        className="w-full h-full absolute inset-0"
+        src="https://dfvc.com/wp-content/themes/dfvc/dist/images/medium_circle.svg"
+        alt=""
+      />
+      <img
+        className="full h-full absolute inset-0"
+        src="https://dfvc.com/wp-content/themes/dfvc/dist/images/small_circle.svg"
+        alt=""
+      />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <img src={logo} className="w-32" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
