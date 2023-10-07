24b1fc9971fb23d9e3676e4ae794fb4c8c53fcd8
Naimur Reza <naimur.rezaa@gmail.com>
2023-10-07 12:28:05 +0600

    done: about page

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import user from "../assets/dustin.jpg";
+import autograph from "../assets/autograph.png";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="lg:flex  max-w-6xl gap-7  ">
+      <img
+        className="border-[10px] border-white/10"
+        src={user}
+        alt=""
+        srcset=""
+      />
+      <div className="space-y-10 pt-4">
+        <h1 className="text-5xl  text-white/80 tracking-widest -translate-x-[75px]">
+          DUSTIN FARRELL
+        </h1>
+        <p className="text-white/40  font-light  tracking-wide">
+          Creating images that awe and entertain has been Dustin Farrell’s focus
+          throughout his career. A veteran in nearly all genres of the film and
+          television industry, Dustin has developed a unique look that is
+          unmistakable. He specializes in slow motion cinematography and motion
+          control time-lapse. He is an Owner/Operator of the Vision Research
+          Phantom Flex4K. In addition we also own and operate a Sony FS7, Sony
+          FS5, Red Dragon, Freefly Mōvi Pro system, Highsight Cablecam, motion
+          control camBLOCK Adventure and Dana Dolly. He is a certified Phantom
+          Tech and has underwater Camera Op experience.
+        </p>
+        <img src={autograph} alt="autograph" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -33,28 +33,31 @@ const Home = () => {
 
   return (
     <div
-      className="w-[480px] h-[480px] relative lense group transition-transform transform"
+      className="w-[480px] h-[480px] relative lense group transition-transform transform disabled select-none"
       onMouseMove={handleMouseMove}>
       <img
+        draggable={false}
         className="w-full h-full lense-large"
         src="https://dfvc.com/wp-content/themes/dfvc/dist/images/large_circle.svg"
         alt=""
         style={{ transform: `rotate(${rotations.large}deg)` }}
       />
       <img
+        draggable={false}
         className="w-full h-full absolute inset-0 lense-medium"
         src="https://dfvc.com/wp-content/themes/dfvc/dist/images/medium_circle.svg"
         alt=""
         style={{ transform: `rotate(${rotations.medium}deg)` }}
       />
       <img
+        draggable={false}
         className="full h-full absolute inset-0 lense-small"
         src="https://dfvc.com/wp-content/themes/dfvc/dist/images/small_circle.svg"
         alt=""
         style={{ transform: `rotate(${rotations.small}deg)` }}
       />
       <div className="absolute inset-0 flex items-center justify-center logo">
-        <img src={logo} className="w-32" alt="" />
+        <img draggable={false} src={logo} className="w-32" alt="" />
       </div>
     </div>
   );
